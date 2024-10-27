796528bf62284bd3b3e118ee23320ce3df9b02cf
Krypton654321 <harshitchauhan123456@gmail.com>
2024-10-27 15:40:47 +0530

    Changes made

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,6 +3,7 @@ import PasswordInput from "../../components/Input/PasswordInput";
 import { useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axiosInstance";
+import bgimg from "../../assets/images/bg-image.png";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -58,8 +59,14 @@ const Login = () => {
       <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
 
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
-        <div className="w-2/4 h-[90vh] flex items-end bg-login-bg-img bg-cover bg-center rounded-lg p-10 z-50">
-          <div>
+        <div className="relative w-2/4 h-[90vh] rounded-lg overflow-hidden">
+          <img
+            src={bgimg} // replace bgimg with the actual path of the image file
+            alt="Travel background"
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute bottom-10 left-10 z-10">
             <h4 className="text-5xl text-white font-semibold leading-[58px]">
               Capture Your <br /> Journeys
             </h4>
--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -3,6 +3,7 @@ import PasswordInput from "../../components/Input/PasswordInput";
 import { useNavigate } from "react-router-dom";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axiosInstance";
+import signupImg from "../../assets/images/signup-bg-img.png"
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -65,11 +66,16 @@ const SignUp = () => {
       <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
 
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
-        <div className="w-2/4 h-[90vh] flex items-end bg-signup-bg-img bg-cover bg-center rounded-lg p-10 z-50">
-          <div>
+        <div className="relative w-2/4 h-[90vh] rounded-lg overflow-hidden">
+          <img
+            src={signupImg} // replace signupImg with the actual path of the image file
+            alt="Signup background"
+            className="w-full h-full object-cover"
+          />
+
+          <div className="absolute bottom-10 left-10 z-10">
             <h4 className="text-5xl text-white font-semibold leading-[58px]">
-              Join the <br />
-              Adventure
+              Join the <br /> Adventure
             </h4>
             <p className="text-[15px] text-white leading-6 pr-7 mt-4">
               Create an account to start documenting your travels and preserving
